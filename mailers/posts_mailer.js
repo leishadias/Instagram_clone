2fc304744f7a25effbe6ac7606dcddadc89006a6
leishadias <68033650+leishadias@users.noreply.github.com>
2023-08-26 13:50:02 +0530

    Update posts_mailer.js

--- mailers/posts_mailer.js
+++ mailers/posts_mailer.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 exports.newPost = async function(post){
     let htmlString = await nodemailer.renderTemplate({post:post},'/posts/new_post.ejs');
     nodemailer.transporter.sendMail({
-       from: 'leid00123456@gmail.com',
+       from: '******@gmail.com',
        to: post.user.email,
        subject: "New Post Uploaded",
        html: htmlString 
